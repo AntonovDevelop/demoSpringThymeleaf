3643210db8eb2713fc76cd559874e3e126d2e71e
Antonov Dmitriy <antonov04092002@mail.ru>
2024-05-28 20:54:28 +0400

    last

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.data.SearchDTO;
 import com.example.demo.service.WeatherInteractor;
 import com.example.demo.service.WeatherService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -17,9 +20,15 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("search", new SearchDTO(""));
         var data = weatherService.getApiData();
         var weatherInteractor = new WeatherInteractor();
         model.addAttribute("current", weatherInteractor.getTemperatures(data));
         return "index";
+    }
+
+    @PostMapping("/find")
+    public String find(@ModelAttribute SearchDTO searchDTO, Model model) {
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/example/demo/data/SearchDTO.kt
@@ -0,0 +1,5 @@
+package com.example.demo.data
+
+data class SearchDTO(
+    val text: String
+)
